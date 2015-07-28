4422a709a52ad3aab2a9b074f0c2b2e7c88d27e1
BrendonPierson <brendonpierson@gmail.com>
2015-07-28 12:55:58 -0500

    added delete button to songs

--- scripts.js
+++ scripts.js
@@ -1,50 +1,45 @@
-var songs = [];
+$(document).ready(function() { 
+	var info1HTML = "";
+	var info2HTML = "";
+  $.ajax({
+    url: "songs.json"
+	  }).done(function(data){
+	    for (var i = 0; i < data.songs.length; i++){
+	    	info1HTML += "<div class='song'><h3>" + data.songs[i].name + "</h3>" + 
+	    							"<p>Artist: " + data.songs[i].artist + "</p>" +
+	    							"<p>On the album: " + data.songs[i].album + "</p>" + 
+	    							'<button type="button" value="delete">Delete Song</button></div>';					
+	    }
+	    $("#info").prepend(info1HTML);
+	  });
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+	  $("#addMoreSongs").click(function(){
+	    	console.log("clicked")
+	    	$.ajax({
+    			url: "songs2.json"
+	  		}).done(function(data){
+	  			console.log(data.songs.length)
+	    		for (var i = 0; i < data.songs.length; i++){
+	    		info2HTML += info1HTML + "<div class='song'><h3>" + data.songs[i].name + "</h3>" + 
+	    								"<p>Artist: " + data.songs[i].artist + "</p>" +
+	    								"<p>On the album: " + data.songs[i].album + "</p>" + 
+	    								'<button type="button" value="delete">Delete Song</button></div>';
+	   		  }
+	    $("#info").html(info2HTML);
+	    });
+		});
+		$("#info").on('click', "button[value='delete']", function(){
+			console.log("clicked delete")
+			$(this).parent().remove();
+		})
+  });
 
-// Each student must add one song to the beginning and the end of the array.
-songs.unshift("Thunder Road - by Bruce Springsteen on the album Born to Run");
-songs[songs.length] = "Lonsome - by Dr. Dog on the album Be the Void";
-
-// Loop over the array and remove any words or characters that obviously don't belong.
-function charRemove(char, replacement) {
-	for (var i = 0; i < songs.length; i++) {
-		var charIndex = -1;
-		var charIndex = songs[i].indexOf(char);
-		if (charIndex !== -1) {
-			songs[i] = songs[i].slice(0, charIndex) + replacement + songs[i].slice(charIndex + 1, songs[i].length);
-		}
-	}
-}
-
-charRemove("@")
-charRemove("(")
-charRemove("!")
-charRemove(">", "-")
+// $( "#dataTable tbody" ).on( "click", "tr", function() {
+//   console.log( $( this ).text() );
+// });
 
 
-// Must add each string to the DOM in index.html in the main content area.
-html = "";
 
-for (var i = 0; i < songs.length; i++){
-	html += '<div class="songInfo">' + songs[i] + "</div>";
-}
-
-document.getElementById("info").innerHTML = html;
-
-// Experimenting with object song notation
-function Song(name, artist, album) {
-	this.name = name;
-	this.artist = artist;
-	this.album = album;
-}
-
-var dd = new Song("Lonesome", "Dr.Dog", "Be the Void");
-console.log(dd.name)
 
 
 
